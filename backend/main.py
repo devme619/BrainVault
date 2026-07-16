e2d9a402191e70e1677db6f20ffd97313dab4cb3
devme619 <f2016404p@alumni.bits-pilani.ac.in>
2026-07-16 16:34:19 +0530

    build crud apis

--- backend/main.py
+++ backend/main.py
@@ -9,11 +9,40 @@ def greet():
     return "Welcome to BrainVault"
 
 products = [
-    Product(1, "phone", "budget phone", 99, 10),
-    Product(2, "laptop", "gaming laptop", 999, 6)
+    Product(id= 1, name= "phone", description= "budget phone",price=  99,quantity=  10),
+    Product(id= 2, name= "laptop", description= "gaming laptop",price=  999,quantity=  6),
+    Product(id= 3, name= "watch", description= "smart watch",price=  200,quantity=  16),
+
 ]
 
 @app.get("/products")
-
 def get_all_products():
-    return "all Products"
+    return products
+
+@app.get("/product/{id}")
+def get_product_by_id(id:int):
+    for product in products:
+        if product.id == id:
+            return product
+    return "Product not found"
+
+@app.post("/product")
+def add_product(product: Product):
+    products.append(product)
+    return product
+
+@app.put("/product")
+def update_product(id:int, product:Product):
+    for i in range(len(products)):
+        if products[i].id ==id:
+            products[i] = product
+            return "Product added successfully"
+    return "No product found"
+
+@app.delete("/product")
+def delete_product(id:int):
+    for i in range(len(products)):
+        if products[i].id == id:
+            del products[i]
+            return "Product Deleted"
+    return "Product not found"
