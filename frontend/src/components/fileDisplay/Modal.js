7797fe2ffc92828334b957949c2d08fc2f0a1e14
devme619 <f2016404p@alumni.bits-pilani.ac.in>
2026-07-19 13:45:23 +0530

    handled form values

--- frontend/src/components/fileDisplay/Modal.js
+++ frontend/src/components/fileDisplay/Modal.js
@@ -1,14 +1,29 @@
+import { useRef, useState } from "react";
 import close from "../../assests/icons/cross-circle.svg";
 
 const Modal = ({ heading, setIsModalOpen }) => {
+  const topic = useRef(null);
+  const description = useRef(null);
+
   const closeModal = () => {
     setIsModalOpen(false);
+  };
+
+  const handleOnClick = (e) => {
+    e.preventDefault();
+    console.log({
+      topic: topic.current.value,
+      description: description.current.value,
+    });
   };
 
   return (
     <div>
       <div className="fixed inset-0 z-20 w-full h-full  bg-black bg-opacity-60" />
-      <form className="fixed inset-0 z-30 w-1/3 h-fit mx-auto my-[10%] rounded-lg bg-slate-900 text-white">
+      <form
+        onSubmit={(e) => e.preventDefault()}
+        className="fixed inset-0 z-30 w-1/3 h-fit mx-auto my-[10%] rounded-lg bg-slate-900 text-white"
+      >
         <div className="flex justify-between p-4">
           <h1 className="font-bold text-2xl">{heading}</h1>
           <img
@@ -22,14 +37,19 @@ const Modal = ({ heading, setIsModalOpen }) => {
           <input
             type="text"
             placeholder="Topic of the note"
-            className="m-4 p-2 rounded-lg"
+            ref={topic}
+            className="m-4 p-2 rounded-lg text-black"
           />
           <input
             type="text"
             placeholder="Describe about note"
-            className="m-4 p-2 rounded-lg"
+            ref={description}
+            className="m-4 p-2 rounded-lg text-black"
           />
-          <button className="w-fit mx-auto my-4 px-2 py-1 rounded-lg bg-white text-black">
+          <button
+            onClick={handleOnClick}
+            className="w-fit mx-auto my-4 px-2 py-1 rounded-lg bg-white text-black"
+          >
             Upload Note
           </button>
         </div>
