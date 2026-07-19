82aebb4c330e096c1d0f6818ad408a8e41fe7d77
devme619 <f2016404p@alumni.bits-pilani.ac.in>
2026-07-19 13:46:43 +0530

    removed unused imports

--- frontend/src/components/fileDisplay/Modal.js
+++ frontend/src/components/fileDisplay/Modal.js
@@ -1,4 +1,4 @@
-import { useRef, useState } from "react";
+import { useRef } from "react";
 import close from "../../assests/icons/cross-circle.svg";
 
 const Modal = ({ heading, setIsModalOpen }) => {
