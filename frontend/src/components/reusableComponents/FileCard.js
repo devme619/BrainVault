a67a9a2f1690d60f2b1f4ee175ee092189370828
devme619 <f2016404p@alumni.bits-pilani.ac.in>
2026-07-21 17:38:35 +0530

    build file card

--- frontend/src/components/reusableComponents/FileCard.js
+++ frontend/src/components/reusableComponents/FileCard.js
@@ -1,5 +1,10 @@
-const FileCard = () => {
-  return <div></div>;
+const FileCard = ({ name, description }) => {
+  return (
+    <div className="w-44 h-60 m-4 p-4 bg-slate-400">
+      <h1>{name}</h1>
+      <div>{description}</div>
+    </div>
+  );
 };
 
 export default FileCard;
